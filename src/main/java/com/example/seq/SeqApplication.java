d999096b776992795b5bc208e956090beef0ef2a
Alex <2640178@gmail.com>
2021-06-06 08:27:32 +0300

    Initial commit

---
+++ src/main/java/com/example/seq/SeqApplication.java
@@ -0,0 +1,13 @@
+package com.example.seq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seq/SeqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
